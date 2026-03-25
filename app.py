c6bf0b383ba9be6cc7b94621ad90f210ff5ee9e3
nikku243631 <nikhilbhardwaj5871@gmail.com>
2026-03-25 08:51:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from flask import Flask, render_template, request, jsonify
+from apis import get_ai_response
+
 import os, json, threading, base64, tempfile
 from datetime import datetime
 import pytz
